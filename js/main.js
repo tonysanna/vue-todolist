7d63cc95e82ca35f564eaa48ddc9552d00a532d3
Antonio <antonio.sanna2706@gmail.com>
2021-09-09 16:21:14 +0200

    Avanzamento Html, Css, JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 Vue.config.devtools = true;
+
+const app = new Vue ({
+    el:'#root',
+    data: {
+        newToDo: "",
+        toDos: [
+            'Fare i compiti',
+            'Fare la spesa',
+            'Fare il bucato'
+        ]
+
+    },
+    methods: {
+        addToDo() {
+            if(this.newToDo != "") {
+                this.toDos.push(this.newToDo),
+                this.newToDo = "";
+            }
+        }
+    }
+
+
+})
